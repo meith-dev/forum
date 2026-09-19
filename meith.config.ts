1dc8e39e1dadea5df15882ada5b91f1f0a3bcbe7
Jordan Harrison <jordan@harrison.to>
2026-09-19 14:37:39 +0100

    update to 0.37.5 and use meith theme

--- meith.config.ts
+++ meith.config.ts
@@ -15,25 +15,25 @@ import { defineForumConfig } from '@meith/web/config'
 import {
   BROWSER_THEME_COLOR,
   DARK_TOKENS,
-  defaultMessages,
-  defaultTheme,
+  meithMessages,
+  meithTheme,
   LIGHT_TOKENS,
-} from '@meith/theme-default'
+} from '@meith/theme-meith'
 
 import { INSTALLED_PLUGINS } from './meith.plugins'
 
 export default defineForumConfig({
   themes: {
     default: {
-      key: 'default',
-      title: 'Default',
+      key: 'meith',
+      title: 'Meith',
       tokens: { light: LIGHT_TOKENS, dark: DARK_TOKENS },
       browserThemeColor: BROWSER_THEME_COLOR,
-      theme: defaultTheme,
-      messages: defaultMessages,
+      theme: meithTheme,
+      messages: meithMessages,
     },
   },
-  defaultTheme: 'default',
+  defaultTheme: 'meith',
 
   plugins: INSTALLED_PLUGINS,
 })
